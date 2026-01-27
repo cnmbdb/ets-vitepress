82cb3ffbf1904747da1df1ae1c0b7fa00c929e11
HF <54242938+cnmbdb@users.noreply.github.com>
2026-01-27 19:49:02 +0800

    2555
    2555

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -36,8 +36,20 @@ export default defineConfig({
   },
 
   themeConfig: {
-    // 右上角的导航，增加版本号下拉
+    // 顶部搜索输入框（带 cmd+k 快捷键）
+    search: {
+      provider: 'local'
+    },
+
+    // 顶部导航 + 版本号下拉
     nav: [
+      { text: '指南', link: '/guide/introduction' },
+      { text: '参考', link: '/api/' },
+      {
+        text: 'Vitejs 中文网',
+        link: 'https://vitejs.cn/vitepress/',
+        target: '_blank'
+      },
       {
         text: '1.3.2',
         items: [
